1151ac1eca36a10b28bd0ad3c2191455e5c4326d
bulululululu <117985482+bulululululu@users.noreply.github.com>
2025-04-11 14:46:37 +0200

    Create Quizz_01_V2.ino

---
+++ Quizz_01/Quizz_01_V2.ino
@@ -0,0 +1,86 @@
+//QUIZZ 01 "QUI POND QUOI ?"
+#include <Adafruit_NeoPixel.h>
+
+const int numPixels = 24;
+const int pixelPin = 10;
+Adafruit_NeoPixel ring = Adafruit_NeoPixel(numPixels, pixelPin);
+uint32_t VERT = ring.Color(0x00, 0xFF, 0x00); // r, g, b - vert
+uint32_t ROUGE = ring.Color(0xFF, 0x00, 0x00); // rouge
+uint32_t ETEINT = ring.Color(0x00, 0x00, 0x00); // eteint
+
+const int questions[] = {A0, A1, A2}; // Boutons des questions
+const int reponses[] = {2, 3, 4, 5, 6, 7}; // Boutons des réponses
+const int  attendre = (5);
+
+const int correspondances[][6] = {
+    {-1, -1, -1, -1, 6, 7}, // Solutions question 1 
+    {-1, 3, -1, 5, -1, -1}, // Solutions question 2 
+    {2, -1, 4, -1, -1, -1} // Solutions question 3 
+ };
+const int nbQuestions = sizeof(questions) / sizeof(questions[0]);
+const int nbReponses = sizeof(reponses) / sizeof(reponses[0]);
+const int maxReponses = sizeof(correspondances[0]) / sizeof(correspondances[0][0]);
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(pixelPin, OUTPUT);
+    
+    for (int i = 0; i < nbQuestions; i++) {
+        pinMode(questions[i], INPUT_PULLUP);
+    }
+    for (int i = 0; i < nbReponses; i++) {
+        pinMode(reponses[i], INPUT_PULLUP);
+    }
+}
+
+void loop() {
+    int questionAppuyee = -1;
+    int reponseAppuyee = -1;
+    int nbBoutonsAppuyes = 0;
+
+    for (int i = 0; i < nbQuestions; i++) {
+        if (digitalRead(questions[i]) == LOW) {
+            questionAppuyee = i;
+            nbBoutonsAppuyes++;
+        }
+    }
+    
+    for (int j = 0; j < nbReponses; j++) {
+        if (digitalRead(reponses[j]) == LOW) {
+            reponseAppuyee = reponses[j];
+            nbBoutonsAppuyes++;
+        }
+    }
+    
+    if (nbBoutonsAppuyes == 2 && questionAppuyee != -1 && reponseAppuyee != -1) {
+        bool bonneReponse = false;
+        for (int k = 0; k < maxReponses; k++) {
+            if (correspondances[questionAppuyee][k] == reponseAppuyee) {
+                bonneReponse = true;
+                break;
+            }
+        }
+        if (bonneReponse) {
+            for (int i = 0; i < numPixels; i++) {
+            ring.setPixelColor(i, VERT); // set pixel i to foreground
+            ring.show();                       // actually display it
+            delay(attendre);                         // milliseconds 
+            Serial.println("correcte");
+            } 
+        } else {
+            for (int i = 0; i < numPixels; i++) {
+            ring.setPixelColor(i, ROUGE); // set pixel i to foreground
+            ring.show();                       // actually display it
+            delay(attendre);                         // milliseconds 
+            Serial.println("faux");
+        }
+      } 
+    } else {
+        for (int i = 0; i < numPixels; i++) {
+        ring.setPixelColor(i, ETEINT); // set pixel i to foreground
+        ring.show();                       // actually display it
+        delay(attendre);                         // milliseconds 
+        Serial.println("eteint");
+        } 
+    }
+}
